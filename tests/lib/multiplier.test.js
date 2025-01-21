73570fb1b3078d4c4c81413b8dba4ba5d00c7e84
Nikolay Favorskiy <lordnikolas9091@gmail.com>
2025-01-21 17:41:53 +0100

    Fix build script command and add failing test case for multiplier function

--- tests/lib/multiplier.test.js
+++ tests/lib/multiplier.test.js
@@ -9,4 +9,8 @@ test('Show product of two numbers', () => {
     expect(multiply(-1, 4)).toBe(-4);
 
     expect(multiply(-2, -2)).toBe(4);
+
+    // This test will fail
+    expect(multiply(2, 2)).toBe(5);
+    
 })
